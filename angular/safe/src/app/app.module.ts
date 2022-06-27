270b1e2e7b7df324c3244c7048decbb92f6d3b25
BiancaNasci <bianascig@gmail.com>
2022-06-27 19:58:19 -0300

    up 1

---
+++ angular/safe/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// Route Module
+import { RoutingModule } from './app.router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+
+//Services
+import { BlogService} from './providers/blog.service';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    ErrorPageComponent,
+    HeaderComponent
+  ],
+  imports: [
+    RoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [BlogService],
+  bootstrap: [FormsModule]
+})
+export class AppModule { }
+
